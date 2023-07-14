6aa5c68251f8bc8e1cc15b94fa66850fa922e3e2
George <xsova113@gmail.com>
2023-07-14 02:10:44 -0700

    minor update

--- components/ui/navbar-actions.tsx
+++ components/ui/navbar-actions.tsx
@@ -6,6 +6,7 @@ import ThemeToggle from "./theme-toggle";
 import { useEffect, useState } from "react";
 import useCart from "@/hooks/use-cart";
 import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 const NavbarActions = () => {
   const [isMounted, setIsMounted] = useState(false);
@@ -21,15 +22,16 @@ const NavbarActions = () => {
 
   return (
     <div className="flex gap-x-4 items-center">
-      <Button
-        onClick={() => router.push("/cart")}
-        className="flex items-center px-4 py-2 rounded-full"
+      <Link
+        href={"/cart"}
+        // onClick={() => router.push("/cart")}
+        className="flex items-center bg-black px-4 py-2 rounded-full"
       >
         <ShoppingBag size={20} className="text-white" />
         <span className="ml-2 text-sm font-medium text-white">
           {cart.items.length}
         </span>
-      </Button>
+      </Link>
       <ThemeToggle />
     </div>
   );
